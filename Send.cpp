a33d2d2bc2b2226ee1644e0bcc08cb1671cd0064
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-04-02 14:47:14 +0800

    输入数据由简单的cin改为getline，现在可以输入空格了。发送数据改为持久运行的模式（while(1)）。

--- Send.cpp
+++ Send.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <winsock2.h>
+#include <string>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -27,36 +28,46 @@ int main()
     char *HostIP = "127.0.0.1";
     int HostPort = 1234;
     string HostIPtmp;
-    cout << "请输入目标主机IP：" << endl;
+    cout << "请输入目标主机IP：";
     cin >> HostIPtmp;
     HostIP = &HostIPtmp[0];
-    cout << "请输入目标主机端口：" << endl;
+    cout << "请输入目标主机端口：";
     cin >> HostPort;
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(1234);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // 输入要发送的数据
-    char *data = "\0";
-    string datatmp;
-    cout << "请输入要发送的内容：" << endl;
-    cin >> datatmp;
-    data = &datatmp[0];
+    // 这里需要cin.ignore()
+    // 原因在于下面的getline会获取到上面cin的最后一个回车，导致执行被跳过。
+    // 但如果放在循环内会影响下次循环的字符获取
+    cin.ignore();
 
-    // 发送数据
-    int dataSize = strlen(data);
-    int ret = sendto(sock, data, dataSize, 0, (sockaddr *)&serverAddr, sizeof(serverAddr));
-    if (ret == SOCKET_ERROR)
+    while (1)
     {
-        cout << "发送数据失败！" << endl;
-        closesocket(sock);
-        WSACleanup();
-        return 1;
-    }
+        // 输入要发送的数据
+        char *data = "\0";
+        string datatmp = "";
+        cout << "请输入要发送的内容：";
 
-    cout << "发送数据成功！" << endl;
-    cout << "发送的数据为：" << data << endl;
+        getline(cin, datatmp);
+        // cin >> datatmp;
+        data = &datatmp[0];
+
+        // 发送数据
+        int dataSize = strlen(data);
+        int ret = sendto(sock, data, dataSize, 0, (sockaddr *)&serverAddr, sizeof(serverAddr));
+        if (ret == SOCKET_ERROR)
+        {
+            cout << "发送数据失败！" << endl;
+            closesocket(sock);
+            WSACleanup();
+            return 1;
+        }
+
+        cout << "发送数据成功！" << endl;
+        cout << "发送的数据为：" << data << endl;
+    }
 
     // 关闭 Socket 和 Winsock
     closesocket(sock);
